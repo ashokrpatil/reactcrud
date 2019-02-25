26f30106e7cd49265dee73ad52e0245b55d9e8d6
Ashok Patil <ashokrpatil12345@gmail.com>
2019-02-25 21:34:57 +0530

    first commit

---
+++ src/components/home.component.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+export default class Home extends Component {
+  
+  render() {
+    return (
+        <div style={{ marginTop: 10 }}>
+        
+        <h4>☺ Please click on <b>Create</b> to add business data</h4> <br/>
+        <h4>☺ Please click on <b>Index</b> to view business data</h4> <br/>
+        </div>
+    )
+  }
+}
